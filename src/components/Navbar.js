07b82158a8dc42bc30550e82e411d0b7cd6ccbd7
mmakarios <michel.makarios@gmail.com>
2018-05-21 18:02:34 -0300

    Add media queries to render navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import Media from 'react-media';
+import mediaQueries from '../constants/mediaQueries';
 
 export default class Navbar extends Component {
   render() {
     const activeStyle = { color: 'gray' };
 
+    const navbarStyle = {
+      height: '49px'
+    };
+
     return (
-      <div>
-        <NavLink exact to="/" activeStyle={activeStyle}>
-          Home
-        </NavLink>
-        <NavLink exact to="/" activeStyle={activeStyle}>
-          Moments
-        </NavLink>
-        <NavLink exact to="/" activeStyle={activeStyle}>
-          Notifications
-        </NavLink>
-        <NavLink exact to="/" activeStyle={activeStyle}>
-          Messages
-        </NavLink>
+      <div style={navbarStyle}>
+        <Media query={mediaQueries.medium}>
+          {matches =>
+            matches ? (
+              <div>
+                <span>americanas.com</span>
+              </div>
+            ) : (
+              <div>
+                <NavLink exact to="/" activeStyle={activeStyle}>
+                  Home
+                </NavLink>
+                <NavLink exact to="/" activeStyle={activeStyle}>
+                  Moments
+                </NavLink>
+                <NavLink exact to="/" activeStyle={activeStyle}>
+                  Notifications
+                </NavLink>
+                <NavLink exact to="/" activeStyle={activeStyle}>
+                  Messages
+                </NavLink>
+              </div>
+            )
+          }
+        </Media>
       </div>
     );
   }
---
+++ src/constants/mediaQueries.js
@@ -0,0 +1,13 @@
+const mediaQueries = {
+  small: {
+    maxWidth: 479
+  },
+  medium: {
+    maxWidth: 767
+  },
+  large: {
+    minWidth: 768
+  }
+};
+
+export default mediaQueries;
