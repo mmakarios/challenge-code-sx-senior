56d57fc03233c5cb4bfebcba27c9906e39fd3672
mmakarios <michel.makarios@gmail.com>
2018-05-22 19:02:20 -0300

    Add mobile page width limit

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import TimelineContainer from '../containers/TimelineContainer';
 import ProfileInfo from './ProfileInfo';
 
+import './HomePage.scss';
+
 export default class HomePage extends Component {
   render() {
     return (
-      <div>
+      <div className="home-page">
         <ProfileInfo />
         {/* <TimelineContainer /> */}
       </div>
